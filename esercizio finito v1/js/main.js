ebfbd0cacdb82ff514f82d01c2aaa38e5a66593f
AlessioMaria5 <alessiomaria5@gmail.com>
2022-07-15 11:15:13 +0200

    Fine Esercizio

--- esercizio finito v1/js/main.js
+++ esercizio finito v1/js/main.js
@@ -1,12 +1,12 @@
 let campo = document.getElementById('campo');
-// LEVELS 
 let livello = document.getElementById('diff');
 
-// PUNTEGGIO
 let punteggio = document.getElementById('punteggio');
 let numeroPunteggio = document.createElement('span');
 punteggio.append(numeroPunteggio);
 let punteggioAttuale = '';
+
+let gameOver = false;
 
 // GENERA CELLE e DIFFICOLTA  -----------------------------------
 
@@ -18,18 +18,19 @@ let bottoneStart = document.getElementById('genera');
    
         switch(livelloInterno) {
             case 1:
-                    level(10,100);
+                level(10,100);
                 break;
                           
             case 2:             
-                    level(9,81);
+                level(9,81);
                 break;
             
             case 3:          
-                    level(7,49);
+                level(7,49);
                 break;
             case 4:
-                level(20,400)         
+                level(20,400);
+                break;        
         }    
 })
 
@@ -44,12 +45,12 @@ function generaBombe(numeroCelle) {
             bombe.push(generatorePosizione); 
         }
     }
-   
     return bombe;
 }
 
 function level(y,z) {
     let finalBombe = generaBombe(z)
+    
         for (let i= 1 ;i<=z; i++) {  
             numeroPunteggio.innerHTML = 'il tuo punteggio è 0';
 
@@ -58,27 +59,31 @@ function level(y,z) {
             cella.style.width = `calc(100% /${y})`
             cella.style.height = `calc(100% /${y})`
             campo.append(cella);
+            
             cella.addEventListener('click', 
             function () {
-            if(!(cella.classList.contains('clicked')))
+                
+            if(!(this.classList.contains('clicked')))
             cella.classList.add('clicked');
+            
             if(finalBombe.includes(i)){
+                cella.classList.remove('clicked');
                 cella.classList.add('bomb');
+                
                 alert('Partita Finita! Hai realizzato '+punteggioAttuale+' punti')
-                const allCell = document.querySelectorAll('.cellaBase');
-                    for(const campo of allCell) {
-                        campo.classList.add('pointereve')
+                // const allCell = document.querySelectorAll('.cellaBase');
+                //     for(const stopClick of allCell) {
+                //         campo.classList.add('pointereve')
+                //     }
+                campo.innerHTML = '';
+                // TUTTE BOMBE ROSSE
+                    const currentCell = document.getElementsByClassName('cellaBase');
+                    for(i =0; i<finalBombe.length; i++){
+                        const currentBomb = finalBombe[i];
+                        currentCell[currentBomb].classList.add('bomb');
                     }
-                    for(let i = 0; i < finalBombe.length; i++){
-                        
-                        cella.innerHTML = '<p>BOMBA</p>'
-                    }
-             
-                punteggioAttuale = 0;
+                    punteggioAttuale = 0;
             }
-
-            else if (cella)
-
             if(cella.classList.contains('clicked')){ 
                 punteggioAttuale++
             }
